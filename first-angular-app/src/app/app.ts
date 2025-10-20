dfb031c3a73dbcee8b57fee2adb1db2c6e24a0c3
hhoang1204 <hhoang12042003@gmail.com>
2025-10-20 20:24:34 +0700

    done

---
+++ first-angular-app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar';
+import { Header } from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, AppNavbar, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('first-angular-app');
+}
---
+++ s2-essentials/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { HeaderComponent } from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('s2-essentials');
+}
